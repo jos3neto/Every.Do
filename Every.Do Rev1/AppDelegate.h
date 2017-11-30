e253a0249ecb9120d3004788fd9405044f52d289
jos3barros <barros_j@yahoo.com>
2017-11-30 13:50:32 -0200

    Initial Commit

---
+++ Every.Do Rev1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Every.Do Rev1
+//
+//  Created by Jose on 30/11/17.
+//  Copyright © 2017 Jose. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Every.Do Rev1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Every.Do Rev1
+//
+//  Created by Jose on 30/11/17.
+//  Copyright © 2017 Jose. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
